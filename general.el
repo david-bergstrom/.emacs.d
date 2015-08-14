da2ac9ebce4dd97bf7816b22752bd800da219eb3
David Bergström <david-bergstrom@outlook.com>
2015-08-14 21:59:40 +0200

    Fixed two first start bugs

--- general.el
+++ general.el
@@ -1,11 +1,3 @@
-;; Enable the package manager
-(require 'package)
-(push '("marmalade" . "http://marmalade-repo.org/packages/")
-      package-archives)
-(push '("melpa" . "http://melpa.milkbox.net/packages/")
-      package-archives)
-(package-initialize)
-
 ;; Tramp makes it easy to edit files that are somewhere else.
 (require 'tramp)
 (setq tramp-default-method "scp")
--- init.el
+++ init.el
@@ -12,7 +12,17 @@
 
 ;; Set the custom file to be in the local folder
 (setq custom-file "~/.emacs.d/local/custom.el")
-(load custom-file)
+(if (file-exists-p "~/.emacs.d/local/custom.el")
+    (load custom-file))
+
+;; Enable the package manager
+(require 'package)
+(push '("melpa" . "http://melpa.milkbox.net/packages/") package-archives)
+(push '("marmalade" . "http://marmalade-repo.org/packages/") package-archives)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 (load-user-file "general.el")
 (load-user-file "c.el")
